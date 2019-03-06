f62a403cbcbed94ef6dca3990678ec7547f6687b
sarita2129 <nairsarita2129@gmail.com>
2019-03-07 08:02:40 +1100

    Updates

---
+++ precache-manifest.a26b517c6edaf1dec7ad2378d2da4c04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cdaa1d8648c5244374a3",
+    "url": "/Food_delivery_client/static/css/main.87d71625.chunk.css"
+  },
+  {
+    "revision": "cdaa1d8648c5244374a3",
+    "url": "/Food_delivery_client/static/js/main.cdaa1d86.chunk.js"
+  },
+  {
+    "revision": "3e4c19b1a27de1d28235",
+    "url": "/Food_delivery_client/static/js/runtime~main.3e4c19b1.js"
+  },
+  {
+    "revision": "10c831898389645e569a",
+    "url": "/Food_delivery_client/static/js/2.10c83189.chunk.js"
+  },
+  {
+    "revision": "8f6013329ecb23d65c0c808f5bddcf1c",
+    "url": "/Food_delivery_client/static/media/pizza3.8f601332.jpg"
+  },
+  {
+    "revision": "0c58cb78bf17d42939477ef15dc3ba74",
+    "url": "/Food_delivery_client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Food_delivery_client/precache-manifest.ad96b8cb9b78d10e09176b5f7f5a2ff0.js"
+  "/Food_delivery_client/precache-manifest.a26b517c6edaf1dec7ad2378d2da4c04.js"
 );
 
 workbox.clientsClaim();
